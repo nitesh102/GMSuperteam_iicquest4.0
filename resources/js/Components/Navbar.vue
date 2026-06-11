a636e467daa0cebd1c9ffe8b4afd098fca1c5125
nitesh chaudhary <nyteeshchaudhary@gmail.com>
2026-06-11 13:27:12 +0545

    complai error resolved

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="flex h-20 shrink-0 items-center justify-between border-b border-gray-200 bg-white text-gray-900 px-8 fixed top-0 right-0 z-50 transition-all duration-300 ease-in-out"
-        :style="{ left: sidebarWidth + 'px' }"
+        :style="{ left: sidebarWidth + 'px' }">
         <!-- Left side -->
         <div class="flex items-center gap-4">
             <!-- Mobile Menu Toggle -->
